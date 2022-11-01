2c0d3f5a770f4f54fc4e88c72716f94b8a362435
richujain <richujain1995@gmail.com>
2022-11-01 04:43:44 -0400

    Initial commit after creating the hero section and technologies section.

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+AOS.init({
+  duration: 1200,
+});
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  items: 4,
+  // items change number for slider display on desktop
+
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  autoplayHoverPause: true,
+});
